cc16441f0661770011d6089441b6b998f2b29b48
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-10 21:42:02 -0300

    login

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Button from './components/button';
+import Input from './components/input';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.image} source={require('./assets/home.jpg')} />
+      <Text>Delivery de compras</Text>
+      <Text style={styles.title}>Minha sacola de mercado</Text>
+
+      <Input placeholder='e-mail'/>
+      <Input placeholder='senha' secureTextEntry/>
+
+      <Button>Entrar</Button>
+      
+      <TouchableOpacity>
+        <Text>criar conta</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FCFBFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#121A2C',
+    fontSize: 34,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#FFF',
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 4,
+    marginTop: 16,
+  }, 
+  button:{
+    backgroundColor: '#121A2C',
+    color: '#FFBA26',
+    paddingVertical: 8,
+    paddingHorizontal: 48,
+    borderRadius: 4,
+    marginTop: 16,
+    marginBottom: 16
+  },
+  image: {
+    height: 200,
+    width: '100%',
+    position: 'absolute',
+    top: 0,
+  }
 });
